d0b60f8ea732d53fef7d28285dafa90e99d77eb8
Nada <nadafarrag619@gmail.com>
2026-01-04 19:28:41 +0200

    Initial commit

---
+++ src/test/java/BrowserActions.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class BrowserActions {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        //WebDriver driver1 = new SafariDriver();
+    }
+}
